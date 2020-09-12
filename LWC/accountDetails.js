26af8d176b334ed17e52d61b2f07e461d7dcce01
Sumer Gurjar <45728749+sumergurjar@users.noreply.github.com>
2020-09-12 19:25:46 +0530

    Update accountDetails.js

--- LWC/accountDetails.js
+++ LWC/accountDetails.js
@@ -4,14 +4,12 @@ export default class AccountDetails extends LightningElement {
     @api recordId;
     @api objectApiName = 'Account';
     fields = ['Name','NumberOfEmployees','My_Field__c','Phone', 'BillingStreet', 'BillingCity', 'BillingState', 'BillingPostalCode'];
-    //accId = '0012100000sPBGdAAO';
+    
     custSuccessContacts = [];
     appDevContacts = [];
     contacts = [];
     iterateVal = 0;
-    //@wire(getContactList, { accId: '$accId' }) 
-    ///contacts;
-
+    
     columns = [
         { label: 'FirstName', fieldName: 'FirstName' },
         { label: 'Title', fieldName: 'Title'},
@@ -20,8 +18,7 @@ export default class AccountDetails extends LightningElement {
     connectedCallback() {        
 
         getContactList({accId: this.recordId})
-            .then(result => {
-                console.log('result:'+JSON.stringify(result));
+            .then(result => {                
                 this.contacts = result;
                 for(this.iterateVal = 0; this.iterateVal < result.length; this.iterateVal++) {
                     if(result[this.iterateVal].Title == "Application Developer") {
@@ -31,10 +28,6 @@ export default class AccountDetails extends LightningElement {
                     }
 
                 }
-
-                console.log('custSuccessContacts:'+JSON.stringify(this.custSuccessContacts));
-                console.log('appDevContacts:'+JSON.stringify(this.appDevContacts));
-                
             }).catch(error => { console.log('error:'+error); })
     }
     
